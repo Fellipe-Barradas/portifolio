977b5244e4dc21630ae99e02a29c5aaf76077f7a
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-07-01 21:58:48 -0300

    Adicionado icones de linguagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import Main from "./components/Main"
 import Project from "./components/Project"
-
+import smartConnect from "/projects/display_smart.png"
+import pokedex from "/projects/pokedex_display.png"
 
 
 function App() {
-  const linguagens = ["HTML", "CSS", "JavaScript", "React", "TailwindCSS"]
+  const linguagens = ["bxl_tailwind-css.png", "devicon_nextjs.png","skill-icons_typescript.png"]
+
+  const linguagens2 = ["bxl_tailwind-css.png", "logos_react.png", "skill-icons_typescript.png"]
   return (
     <div className="mt-48 flex flex-col items-center justify-center gap-20">
       <Main/>
       <h1 className="font-bold text-xl mt-20">Projetos</h1>
       <div className="grid grid-cols-2 gap-20 mt-10 ">
-        <Project project="Website Smart Connect" linguagens={linguagens}>Olá</Project>
-        <Project project="Website Smart Connect" linguagens={linguagens}>Olá</Project>
-        <Project project="Website Smart Connect" linguagens={linguagens}>Olá</Project>
-        <Project project="Website Smart Connect" linguagens={linguagens}>Olá</Project>
+        <Project project="Website Smart Connect" linguagens={linguagens}><img src={smartConnect} alt="imagem de projeto smart connect" /></Project>
+        <Project project="Website Smart Connect" linguagens={linguagens2}><img src={pokedex} alt="imagem de projeto pokédex" /></Project>
+  
       
       </div>
     </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,16 @@
+
+
+function displayLanguages(languages) {
+    return languages.map(language => {
+        let url = `/languages/${language}`
+        return <img className=" p-2 m-2 rounded-lg" src={url}></img>
+    })
+}
+
 export default function Project(props) {
     return(
-        <div className="p-3   bg-red-900">
-            <div>{props.linguagens.map(linguagem=>{
-                return <span className="bg-red-500 p-2 m-2 rounded-lg">{linguagem}</span>
-            })}</div>
+        <div className="p-3 flex flex-col items-center">
+            <div className="flex">{displayLanguages(props.linguagens)}</div>
             {props.children}
             <h1 className="text-center">{props.project}</h1>
         </div>
