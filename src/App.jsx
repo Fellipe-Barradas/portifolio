54a2d4bacaf24e344e373da5c88623da1e498f0f
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-07-01 21:40:34 -0300

    Adicionado imagens principais

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import Project from "./components/Project"
 function App() {
   const linguagens = ["HTML", "CSS", "JavaScript", "React", "TailwindCSS"]
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-20">
+    <div className="mt-48 flex flex-col items-center justify-center gap-20">
       <Main/>
-      <h1 className="font-bold text-xl">Projetos</h1>
-      <div className="grid grid-cols-2 gap-20  ">
+      <h1 className="font-bold text-xl mt-20">Projetos</h1>
+      <div className="grid grid-cols-2 gap-20 mt-10 ">
+        <Project project="Website Smart Connect" linguagens={linguagens}>Olá</Project>
+        <Project project="Website Smart Connect" linguagens={linguagens}>Olá</Project>
+        <Project project="Website Smart Connect" linguagens={linguagens}>Olá</Project>
         <Project project="Website Smart Connect" linguagens={linguagens}>Olá</Project>
       
       </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 export default function Project(props) {
     return(
-        <div className="p-20 bg-red-900">
+        <div className="p-3   bg-red-900">
             <div>{props.linguagens.map(linguagem=>{
                 return <span className="bg-red-500 p-2 m-2 rounded-lg">{linguagem}</span>
             })}</div>
